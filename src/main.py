cc92ae16f3d3f1feb578037608341c69236ef167
Camilo Arias Guerrero <camiloalex2000@gmail.com>
2026-07-22 11:55:40 -0500

    fix: refactorizar API_BASE_URL con import.meta.env.PUBLIC_API_URL y habilitar CORS en backend

--- src/main.py
+++ src/main.py
@@ -20,6 +20,7 @@ app.add_middleware(
         "http://127.0.0.1:4321",
         "http://localhost:3000",
         "http://127.0.0.1:3000",
+        "*",
     ],
     allow_credentials=True,
     allow_methods=["*"],
